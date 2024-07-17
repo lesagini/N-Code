f8a5e2d8cf633a772ed432f4e0c9aa86cc4247a8
Emmanuel Sagini <emmanuelsagini2@gmail.com>
2024-07-17 08:28:52 +0300

    wip: fix failing time test

--- tests/test_Parser.cpp
+++ tests/test_Parser.cpp
@@ -7,7 +7,7 @@ TEST_CASE("Parser tests")
 
     std::string sampleData = "$GPRMC,060100.000,A,0144.4683,S,03648.0835,E,2.5,45.0,080724,,A*6C";
     Parser::Data result = parser.parseSentence(sampleData);
-    CHECK(result.timestamp == "Mon, 08 Jul 2024 03:01:00 UTC");
+    CHECK(result.timestamp == "Mon, 08 Jul 2024 06:01:00 UTC");
     CHECK(result.speed == "2.5");
     CHECK(result.course == "45.0");
 }
